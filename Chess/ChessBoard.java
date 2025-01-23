5231e3b849c63be03bf283e700f977ff93f2fbf7
AaronMMcfailLuttrell <126686848+AaronMMcfailLuttrell@users.noreply.github.com>
2025-01-23 13:54:54 -0600

    Refining code
    Code was split up into separate, intermediate, methods. Done for ease of reading and decluttering the main methods. Comments also added for each method.

--- Chess/ChessBoard.java
+++ Chess/ChessBoard.java
@@ -11,60 +11,68 @@ import javax.swing.*;
 
 public class ChessBoard extends JPanel {
 	
-	boolean switchVar;
 	public ChessBoard () {
 		
 		this.setLayout(new GridLayout(8,8));
-		JPanel test[][] = new JPanel[8][8];
-		
 		this.setVisible(true);
 		this.setBackground(Color.WHITE);
+		JPanel Squares[][] = generateBoard();
+		
+		
+		
+	}
+	
+	
+	/*
+	 * Method name: generateBoard: Creates the grid of panels, black and white
+	 * Input: None
+	 * Output: Double array of premade panels
+	 */
+	private JPanel[][] generateBoard() {
+		JPanel temporary[][] = new JPanel[8][8];
 		
 		//Generate JPanels for black and white 
-		for (int i = 0; i < 8; i++) {
-			
-			if (i % 2 == 0) {
-				int colorVal = 0;
-				for (int u = 0; u < 8; u++) {
-					test[i][u] = new JPanel();
-					test[i][u].setSize(new Dimension(50,50));
-					if (colorVal == 0 ) {
-						test[i][u].setBackground(Color.WHITE);
-						colorVal = 1;
+				for (int i = 0; i < 8; i++) {
+					
+					if (i % 2 == 0) {
+						int colorVal = 0;
+						for (int u = 0; u < 8; u++) {
+							temporary[i][u] = new JPanel();
+							temporary[i][u].setSize(new Dimension(50,50));
+							if (colorVal == 0 ) {
+								temporary[i][u].setBackground(Color.WHITE);
+								colorVal = 1;
+							} else {
+								temporary[i][u].setBackground(Color.BLACK);
+								colorVal = 0;
+							}
+							//test[i][u].setLocation(u*50, i*50);
+							temporary[i][u].setVisible(true);
+							this.add(temporary[i][u]);
+							this.repaint();
+						}
 					} else {
-						test[i][u].setBackground(Color.BLACK);
-						colorVal = 0;
+						int colorVal = 1;
+						for (int u = 0; u < 8; u++) {
+							temporary[i][u] = new JPanel();
+							temporary[i][u].setSize(new Dimension(50,50));
+							if (colorVal == 0 ) {
+								temporary[i][u].setBackground(Color.WHITE);
+								colorVal = 1;
+							} else {
+								temporary[i][u].setBackground(Color.BLACK);
+								colorVal = 0;
+							}
+							temporary[i][u].setLocation(u*50, i*50);
+							temporary[i][u].setVisible(true);
+							this.add(temporary[i][u]);
+							this.repaint();
+						}
 					}
-					//test[i][u].setLocation(u*50, i*50);
-					test[i][u].setVisible(true);
-					this.add(test[i][u]);
-					this.repaint();
+					
 				}
-			} else {
-				int colorVal = 1;
-				for (int u = 0; u < 8; u++) {
-					test[i][u] = new JPanel();
-					test[i][u].setSize(new Dimension(50,50));
-					if (colorVal == 0 ) {
-						test[i][u].setBackground(Color.WHITE);
-						colorVal = 1;
-					} else {
-						test[i][u].setBackground(Color.BLACK);
-						colorVal = 0;
-					}
-					test[i][u].setLocation(u*50, i*50);
-					test[i][u].setVisible(true);
-					this.add(test[i][u]);
-					this.repaint();
-				}
-			}
-			
-		}
 		
-		
-		
-		
-		
+		return temporary;
 	}
 	
 	public static void main(String args[]) {
--- Chess/ChessGame.java
+++ Chess/ChessGame.java
@@ -12,6 +12,12 @@ import javax.swing.*;
 
 public class ChessGame extends JFrame {
 	private int windowSize = 500;
+	private double sidePanelRatio = .15; //How much space of the window does the side panel take up
+	JPanel east = new JPanel();
+	JPanel west = new JPanel();
+	JPanel south = new JPanel();
+	JPanel north = new JPanel();
+	
 	public ChessGame() {
 		
 		ChessBoard chessBoard = new ChessBoard();
@@ -23,74 +29,131 @@ public class ChessGame extends JFrame {
 		add(chessBoard, BorderLayout.CENTER);
 		
 		//Change in the future, just test
-		JPanel north = new JPanel();
+		addSidePanels();
+		addWindowResizeSensor();
+		
+	}
+	
+	
+	/*
+	* Method name: whenWindowResized
+	* Description: Automatically adjusts anything that needs to be readjusted when the window is resized
+	* Input: None
+	* Output: None
+	*/
+	private void whenWindowResized() {
+		if (getWidth() > getHeight()) {
+        	setSize(new Dimension(getWidth(), getWidth()));
+        } else {
+        	setSize(new Dimension(getHeight(), getHeight()));
+        }
+        
+        //Resize borders
+        north.setPreferredSize(new Dimension(0,(int)(sidePanelRatio*getWidth())));
+		west.setPreferredSize(new Dimension((int)(sidePanelRatio*getWidth()), 0));
+		south.setPreferredSize(new Dimension(0, (int)(sidePanelRatio*getWidth())));
+		east.setPreferredSize(new Dimension((int)(sidePanelRatio*getWidth()), 0));
+        
+	}
+	
+	/*
+	* Method name: northPanelDesign
+	* Description: Holds the design properties for the north panel (intermediary method)
+	* Input: None
+	* Output: None
+	*/
+	private void northPanelDesign() {
 		north.setVisible(true);
-		north.setBackground(Color.BLUE);
-		JPanel south = new JPanel();
-		south.setVisible(true);
-		south.setBackground(Color.BLUE);
-		JPanel west = new JPanel();
+		north.setBackground(Color.GRAY);
+		north.setPreferredSize(new Dimension(0,(int)(sidePanelRatio*windowSize)));
+	}
+	
+	/*
+	* Method name: westPanelDesign
+	* Description: Holds the design properties for the west panel (intermediary method)
+	* Input: None
+	* Output: None
+	*/
+	private void westPanelDesign() {
 		west.setVisible(true);
-		west.setBackground(Color.BLUE);
-		JPanel east = new JPanel();
+		west.setBackground(Color.GRAY);
+		west.setPreferredSize(new Dimension((int)(sidePanelRatio*windowSize), 0));
+	}
+	
+	/*
+	* Method name: southPanelDesign
+	* Description: Holds the design properties for the south panel (intermediary method)
+	* Input: None
+	* Output: None
+	*/
+	private void southPanelDesign() {
+		south.setVisible(true);
+		south.setBackground(Color.GRAY);
+		south.setPreferredSize(new Dimension(0, (int)(sidePanelRatio*windowSize)));
+	}
+	
+	/*
+	* Method name: eastPanelDesign
+	* Description: Holds the design properties for the east panel (intermediary method)
+	* Input: None
+	* Output: None
+	*/
+	private void eastPanelDesign() {
 		east.setVisible(true);
-		east.setBackground(Color.BLUE);
-		
-		JButton testButton = new JButton("Test button");
-		testButton.setVisible(true);
-		west.add(testButton);
-		
-		JButton testTwo = new JButton ("Test space button");
-		testTwo.setVisible(true);
-		east.add(testTwo);
-		north.setPreferredSize(new Dimension(0,(int)(.10*windowSize)));
-		west.setPreferredSize(new Dimension((int)(.10*windowSize), 0));
-		south.setPreferredSize(new Dimension(0, (int)(.10*windowSize)));
-		east.setPreferredSize(new Dimension((int)(.10*windowSize), 0));
-		
+		east.setBackground(Color.GRAY);
+		east.setPreferredSize(new Dimension((int)(sidePanelRatio*windowSize), 0));
+	}
+	
+	/*
+	* Method name: addSidePanels
+	* Description: Function to handle calling intermediary build methods, and adding them to the frame.
+	* Input: None
+	* Output: None
+	*/
+	private void addSidePanels() {
+		northPanelDesign();
+		eastPanelDesign();
+		southPanelDesign();
+		westPanelDesign();
 		add(north, BorderLayout.NORTH);
 		add(south, BorderLayout.SOUTH);
 		add(east, BorderLayout.EAST);
 		add(west, BorderLayout.WEST);
 		revalidate();
-		
+	}
+	
+	/*
+	* Method name: addWindowResizeSensor
+	* Description: Only needs to be called once. Adds the resize listener (ADD ANYTHING THAT NEEDS TO BE CHANGED WHEN RESIZED TO whenWindowResized)
+	* Input: None
+	* Output: None
+	*/
+	private void addWindowResizeSensor() {
 		//-Change Later
-				Timer resizeTimer = new Timer();
+		Timer resizeTimer = new Timer();
 
-		        addComponentListener(new ComponentAdapter() {
-		            private TimerTask resizeTask;
+        addComponentListener(new ComponentAdapter() {
+            private TimerTask resizeTask;
 
-		            @Override
-		            public void componentResized(ComponentEvent e) {
-		                if (resizeTask != null) {
-		                    resizeTask.cancel(); // Cancel the previous task
-		                }
+            @Override
+            public void componentResized(ComponentEvent e) {
+                if (resizeTask != null) {
+                    resizeTask.cancel(); // Cancel the previous task
+                }
 
-		                resizeTask = new TimerTask() {
-		                    @Override
-		                    public void run() {
-		                        SwingUtilities.invokeLater(() -> {
-		                            if (getWidth() > getHeight()) {
-		                            	setSize(new Dimension(getWidth(), getWidth()));
-		                            } else {
-		                            	setSize(new Dimension(getHeight(), getHeight()));
-		                            }
-		                            
-		                            //Resize borders
-		                            north.setPreferredSize(new Dimension(0,(int)(.10*getWidth())));
-		                    		west.setPreferredSize(new Dimension((int)(.10*getWidth()), 0));
-		                    		south.setPreferredSize(new Dimension(0, (int)(.10*getWidth())));
-		                    		east.setPreferredSize(new Dimension((int)(.10*getWidth()), 0));
-		                            
-		                        });
-		                    }
-		                };
+                resizeTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        SwingUtilities.invokeLater(() -> {
+                            whenWindowResized();
+                        });
+                    }
+                };
 
-		                // Schedule the task to run after a delay (e.g., 300 ms)
-		                resizeTimer.schedule(resizeTask, 300);
-		            }
-		        });
-		
+                // Schedule the task to run after a delay (e.g., 300 ms)
+                resizeTimer.schedule(resizeTask, 300);
+            }
+        });
 	}
 	
 	public static void main(String args[]) {
